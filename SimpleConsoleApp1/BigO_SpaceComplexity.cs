f6663be13d6fa527afaebd08674edffc8e154f78
udaykumarburgula2k <130174397+udaykumarburgula2k@users.noreply.github.com>
2024-07-07 10:32:22 +0530

    BigOReading

---
+++ SimpleConsoleApp1/BigO_SpaceComplexity.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleApp
+{
+    internal class BigO_SpaceComplexity
+    {
+        public static void sumOfNumberUntilZeroWithRecursion(int givenNumber) {
+            Console.WriteLine("sumOfNumberUntilZeroWithRecursion");
+            Console.WriteLine(sum(givenNumber));
+        }
+
+        public static int sum(int n) { 
+            if (n <= 0) { return 0; }
+            return n + sum(n - 1);
+        }
+
+        public static void pairSumSequenceWithoutRecursion(int givenNumber) {
+            Console.WriteLine("pairSumSequenceWithoutRecursion");
+            Console.WriteLine(pairSumSequence(givenNumber));
+        }
+
+        public static int pairSumSequence(int n)
+        {
+           int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += pairSum(i, i + 1);
+            }
+            return sum;
+        }
+
+        public static int pairSum(int n1, int n2)
+        {
+            return  n1 + n2;
+        }
+
+
+        public static void dropTheConstants(int givenNumber)
+        {
+            Console.WriteLine("pairSumSequenceWithoutRecursion");
+            Console.WriteLine((doDropTheConstants(givenNumber)==1)? "Min changed!!": "Max changed!!");
+        }
+
+        public static void dropTheConstantsFineTune(int givenNumber)
+        {
+            Console.WriteLine("pairSumSequenceWithoutRecursion");
+            Console.WriteLine((doFineTuneDropTheConstants(givenNumber) == 1) ? "Min changed!!" : "Max changed!!");
+        }
+
+        public static int doDropTheConstants(int age)
+        {
+            int min = 21;
+            int max = 60;
+            int operation = 0;
+            for (int i = 0; i < age; i++) {
+                if (age < min) { min = age; operation = 1; } // Section#A
+                if (age > max) { max = age; operation = 2; } // Section#B
+            }
+            // Timecomplexity : A*B
+            return operation;
+        }
+
+        public static int doFineTuneDropTheConstants(int age)
+        {
+            int min = 21;
+            int max = 60;
+            int operation = 0;
+            for (int i = 0; i < age; i++)
+            {
+                if (age < min) { min = age; operation = 1; }// Section#A
+            }
+            for (int i = 0; i < age; i++)
+            {
+                if (age < min) { min = age; operation = 1; }// Section#B
+            }
+            // Timecomplexity : A+B
+            return operation;
+        }
+
+
+
+    }
+}
--- SimpleConsoleApp1/Program.cs
+++ SimpleConsoleApp1/Program.cs
@@ -17,6 +17,32 @@ namespace SimpleConsoleApp
             // SumOfDigitsForGivenNumber.CalculateSumOfDigits();
             // Swap2NumberWithThirdNumber.DoSwap2NumberWithThirdNumber();
             // Swap2NumberWithThirdNumber.DoSwap2NumberWithoutThirdNumber();
+
+            //BigO_SpaceComplexity.sumOfNumberUntilZeroWithRecursion(1000);
+            //BigO_SpaceComplexity.pairSumSequenceWithoutRecursion(1000);
+            // BigO_SpaceComplexity.dropTheConstants(12); // A*B
+            // BigO_SpaceComplexity.dropTheConstants(60);
+            //BigO_SpaceComplexity.dropTheConstantsFineTune(12); // A+B
+            //BigO_SpaceComplexity.dropTheConstantsFineTune(60);
+
+            // Multipart Algorithms: Add vs Multiply 
+            /* 
+             * // (A) do this, when all done, do that = Add the runtimes
+                for(int a: arrA){
+                    print(a);
+                }
+                for(int b: arrB){
+                    print(b);
+                }
+            * // (B) do this, for each time, do that = multiply the runtimes
+                for(int a: arrA){
+                    for(int b: arrB){
+                        print(a + ","+ b);
+                    }
+                }
+             */
+
+            Console.WriteLine("Execution Done!!");
         }
     }
 }
